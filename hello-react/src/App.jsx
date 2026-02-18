d16e48288314068d295bd21eeea0a207db31286c
Alyssa <lyssag0512@gmail.com>
2026-02-18 10:14:55 -0500

    Initial commit

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+
+ return ( 
+
+   <div> 
+
+     <h1>Hello React!</h1> 
+
+     <p>This is your first React component.</p> 
+
+   </div> 
+
+ ); 
+
+} 
+
+ 
+
+export default App
